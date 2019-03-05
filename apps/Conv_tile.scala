70693b4d9fa10d649029928886e1e90644437325
YuMingChang <yumingchang8587@gmail.com>
2019-03-06 00:44:49 +0800

    Fix Conv / Conv_tile issues

--- apps/Conv_tile.scala
+++ apps/Conv_tile.scala
@@ -1,12 +1,13 @@
 import spatial.dsl._
 
-@spatial object Conv extends SpatialApp {
+@spatial object Conv_test extends SpatialApp {
 
   def main(args: Array[String]): Unit = {
     type T = FixPt[ TRUE, _16, _16 ]
 
 	// parameters
-	val input_size  = 16 // current same for sliding window and tile size, may change
+    val input_size = 64
+	val tile_size  = 16 // current same for sliding window and tile size, may change
 	val kernel_size = 3
 
 	// load data
@@ -27,28 +28,33 @@ import spatial.dsl._
 		val sldwin = RegFile[T]( kernel_size )
 
 		// allocate memory for input output and kernel
-		val rawdata = SRAM[T]( input_size ) // tile size
-		val result  = SRAM[T]( input_size ) // tile size
+		val rawdata = SRAM[T]( tile_size ) // tile size
+		val result  = SRAM[T]( tile_size ) // tile size
 		val kersram = SRAM[T]( kernel_size )
 
 		// load tile memory
-		rawdata load srcmem // need to add offset if tiling
 		kersram load kermem
 
-		Foreach( input_size by 1 ){ j =>
+        Foreach( input_size by tile_size ){ i =>
 
-			// Shift next element into sliding window
-			sldwin <<= rawdata( j )
+			rawdata load srcmem( i::i+tile_size )
 
-			// compute convolution
-			result(j) = Reduce( Reg[T] ( 0.to[T] ) )( kernel_size by 1 ){ i=>
-				sldwin( i ) * kersram( i )
-			}{ _ + _ }
+			Foreach( tile_size by 1 ){ j =>
+
+				// Shift next element into sliding window
+				sldwin <<= rawdata( j )
+
+				// compute convolution
+				result(j) = Reduce( Reg[T] ( 0.to[T] ) )( kernel_size by 1 ){ i=>
+					sldwin( i ) * kersram( i )
+				}{ _ + _ }
+
+            }
+
+		    // store result
+		    dstmem( i::i+tile_size ) store result
 
 		}
-
-		// store result
-		dstmem store result
 
 	}
 
